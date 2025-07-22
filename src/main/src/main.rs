9ddd8f1d519f967926c78ddc454fd9bdc5e06849
Jorge Paravicini <24812622+jorgeparavicini@users.noreply.github.com>
2025-07-22 21:15:12 +0200

    chore: cleanup main function

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -1,62 +1,12 @@
-use std::thread::sleep;
-use std::time::Duration;
-use flux_ecs::commands::Commands;
-use flux_ecs::component::Component;
-use flux_ecs::query::Query;
-use flux_ecs::resource::{Res, Resource};
-use flux_ecs::schedule::ScheduleLabel;
 use flux_ecs::schedule::ScheduleLabel::Initialization;
 use flux_ecs::world::World;
 use flux_renderer::RendererPlugin;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let mut world = World::new();
     world.add_plugin(RendererPlugin);
     world.run_system(&Initialization);
     sleep(Duration::from_secs(1));
-}
-
-struct Time {
-    seconds: f32,
-}
-
-impl Resource for Time {}
-
-fn test_system(r: Res<Time>) {
-    println!("Seconds elapsed: {}", r.seconds);
-}
-
-fn empty_system() {}
-
-#[derive(Debug)]
-struct TestComponent {}
-
-impl Component for TestComponent {}
-
-fn query_system(q: Query<&TestComponent>) {
-    for item in q {
-        println!("Querying TestComponent: {item:?}");
-    }
-}
-
-fn command_system(mut commands: Commands) {
-    commands.insert_resource(Time { seconds: 0.0 });
-}
-
-fn test() {
-    let mut world = World::new();
-    world.add_resource(Time { seconds: 42.0 });
-    world.spawn((TestComponent {},));
-
-    //Add a system that prints the entity index
-    world.add_system(ScheduleLabel::Main, test_system);
-    world.add_system(ScheduleLabel::Main, empty_system);
-    world.add_system(ScheduleLabel::Main, query_system);
-    world.add_system(ScheduleLabel::Main, command_system);
-
-    // Run all systems
-    for _ in 0..10 {
-        world.get_resource_mut::<Time>().unwrap().seconds += 1.0;
-        //world.run_systems();
-    }
 }
